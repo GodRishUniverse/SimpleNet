738457b039dc69d89e527669ea2020d9c3f11a68
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-03-12 10:42:04 -0600

    tensor implementation is wrong and needs to be reworked
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- src/Tensor.h
+++ src/Tensor.h
@@ -8,39 +8,7 @@ private:
     double * data;
 public:
     // constructor when size and data are provided
-    Tensor(int * size, double * data): size(size), data(data) {
-    };
 
-    // constructor when only size is provided
-    Tensor(int * size): size(size) {
-        int totalSize = 1;
-        for (int i = 0; i < sizeof(size)/sizeof(size[0]); i++) {
-            totalSize *= size[i];
-        }
-        this->data = new double[totalSize];
-        for (int i = 0; i < totalSize; i++) {
-            this->data[i] = 0;
-        }
-    };
-
-    double getData(int * indices) const {
-        if (sizeof(indices)/sizeof(indices[0] != sizeof(size)/sizeof(size[0]))) {
-            throw std::runtime_error("Index size mismatch");
-            return 0;
-        }
-
-        // TODO: implement index checking and one dimensional index checking
-    }
-
-
-    int * getSize() const {
-        return this->size;
-    }
-
-    ~Tensor() {
-        delete[] data;
-        delete[] size;
-    };
 };
 
 #endif
